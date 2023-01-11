abeb2d0772c34d94ef918ada87c97abe208a4e11
Canthar90 <d.cieslak90@gmail.com>
2023-01-11 22:54:34 +0100

    Added object detection

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import cv2
 import time
+from emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(3)
 
 frist_frame = None
 cont_fact = 0
+status_list = [0, 0]
+
 while True:
-    
+    status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
@@ -28,10 +31,23 @@ while True:
                                        cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         if cv2.contourArea(contour) < 11000:
+            status_flag = False
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
+        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
+        if rectangle.any() and  not status_flag:
+            status = 1
+            
         
+           
+    status_list.append(status)
+    status_list = status_list[-2:]
+    
+    if status_list[0] == 0 and status_list[1] == 1:
+        send_email()
+        
+    status_list.append(status)
+    
     cv2.imshow("Video", frame)
             
     key = cv2.waitKey(1)
