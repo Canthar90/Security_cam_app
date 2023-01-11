41759f24d027b8f0ad5a3f4c2a6150f8711e0446
Canthar90 <d.cieslak90@gmail.com>
2023-01-11 22:07:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+import cv2
+import time
+
+video = cv2.VideoCapture(0)
+time.sleep(3)
+
+frist_frame = None
+cont_fact = 0
+while True:
+    
+    check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+    
+    if frist_frame is None and cont_fact < 10:
+        cont_fact += 1
+        continue
+    elif frist_frame is None and cont_fact >= 10:
+        frist_frame = gray_frame_gau
+        
+    delta_frame = cv2.absdiff(frist_frame, gray_frame_gau)
+    
+    
+    thresh_frame = cv2.threshold(delta_frame, 48, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL,
+                                       cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) < 11000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
+        
+    cv2.imshow("Video", frame)
+            
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+
+video.release()
+
