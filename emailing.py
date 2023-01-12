688431e97f75c0e8901ced228d2a08a05016c276
Canthar90 <d.cieslak90@gmail.com>
2023-01-12 15:00:12 +0100

    Added email with attachment sending backend

--- emailing.py
+++ emailing.py
@@ -1,2 +1,46 @@
-def send_email():
+from dotenv import load_dotenv
+import smtplib, ssl
+from email import encoders
+import os
+from email.message import EmailMessage
+import imghdr
+
+
+load_dotenv("global.env")
+LOGIN = os.getenv("EMAIL_USERNAME")
+PASS = os.getenv("EMAIL_PASS")
+HOST = "smtp.gmail.com"
+PORT = 587
+
+reciver = LOGIN
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer!")
+    
+    # loading image file 
+    with open(image_path, "rb") as file:
+        content = file.read()
+    
+    email_message.add_attachment(content, maintype="image",
+                                 subtype=imghdr.what(None, content))
+    
+    
+    
+    gmail = smtplib.SMTP(HOST, PORT)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(LOGIN, PASS)
+    gmail.sendmail(LOGIN, reciver, email_message.as_string())
+    gmail.quit()
+
+        
+    
     print("Email was send")
+    
+    
+    
+    
+if __name__=="__main__":
+    send_email(image_path="images\image1.png")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(0)
 time.sleep(3)
@@ -8,8 +9,9 @@ time.sleep(3)
 frist_frame = None
 cont_fact = 0
 status_list = [0, 0]
-
+count = 1
 while True:
+
     status = 0
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -31,12 +33,16 @@ while True:
                                        cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         if cv2.contourArea(contour) < 11000:
-            status_flag = False
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0))
-        if rectangle.any() and  not status_flag:
+        if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/image{count}.png", frame)
+            count =  count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images)/2)
+            image_with_object = all_images[index]
             
         
            
@@ -44,7 +50,7 @@ while True:
     status_list = status_list[-2:]
     
     if status_list[0] == 0 and status_list[1] == 1:
-        send_email()
+        send_email(image_with_object)
         
     status_list.append(status)
     
